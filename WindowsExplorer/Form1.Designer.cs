db92164a030555b743803f0ee508ea8b82650d2a
ahmedm0heb <moheb413@gmail.com>
2020-07-26 10:01:45 +0100

    Add project files.

---
+++ WindowsExplorer/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WindowsExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(230, 450);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.Enter += new System.EventHandler(this.treeView1_Enter);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "118-1189137_folder-png-open-yellow-folder-icon-png-transparent.png");
+            this.imageList1.Images.SetKeyName(1, "Document.ico");
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(267, 188);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(507, 250);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Enter += new System.EventHandler(this.listView1_Enter);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "FileName ";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Length ";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Access";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Type ";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Creation Time ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(440, 113);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(267, 113);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(257, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 42);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(617, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(267, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(507, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(257, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(121, 39);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Drives";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(440, 95);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(121, 39);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "View";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WindowsExplorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ WindowsExplorer/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsExplorer
+{
+    public partial class Form1 : Form
+    {
+        string SelectedDrive, SelectedPath;
+        public Form1()
+        {
+            InitializeComponent();
+
+                                                            //we need to get drives names 
+            getDrives(); 
+            comboBox1.Text = "C:\\";
+
+                                                            //fill the combobox of the type of icons view
+            comboView(); 
+            comboBox2.Text = "Details";
+        }
+        void comboView()
+
+        { 
+            comboBox2.Items.Add("LargeIcon"); 
+            comboBox2.Items.Add("Details"); 
+            comboBox2.Items.Add("SmallIcon");
+            comboBox2.Items.Add("List"); 
+            comboBox2.Items.Add("Tile"); 
+        }
+
+                                                           //get all drives on the pc and put it into the combobox
+        void getDrives(){
+            foreach (string drive in Directory.GetLogicalDrives()) 
+            {
+                comboBox1.Items.Add(drive); 
+            }
+        }
+
+                                                           //evry folder of the selected drive will appear as a loop 
+        void getFolders() 
+        {
+            SelectedDrive = comboBox1.Text; 
+            DirectoryInfo Dir = new DirectoryInfo(SelectedDrive);
+            treeView1.Nodes.Clear();
+            foreach (DirectoryInfo Folder in Dir.GetDirectories())
+            { 
+                treeView1.Nodes.Add("", Folder.Name, 0, 0); 
+            } 
+        }
+
+        //
+        void getFiles(string strPath)
+        {
+            ListViewItem lvi;
+            DirectoryInfo Dir = new DirectoryInfo( /*this is the name of the drive */ SelectedDrive + /*this is the path of the drive*/ strPath);
+            listView1.Items.Clear(); 
+            try
+            { 
+                foreach (FileInfo files in Dir.GetFiles())
+                { 
+                    lvi = listView1.Items.Add(files.Name, 1);
+                    double FileLength = files.Length;
+                    lvi.SubItems.Add(Math.Round(FileLength /1000, 3) + " KB");
+                    lvi.SubItems.Add(files.LastAccessTime.ToString());
+                    lvi.SubItems.Add(files.Extension);
+                    lvi.SubItems.Add(files.CreationTime.ToString());
+                }
+            }
+            catch (Exception ex) 
+            { 
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            getFolders();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            SelectedPath = e.Node.FullPath;
+            /*send the parameter to the function getfiles*/ getFiles(SelectedPath);
+            TreeNode node;
+            DirectoryInfo Dir = new DirectoryInfo(/*this is the name of the drive */ SelectedDrive + /*this is the path of the drive*/ SelectedPath);
+            try 
+            { 
+                foreach (DirectoryInfo folder in Dir.GetDirectories())
+                {
+                    node = new TreeNode(folder.Name, 0, 0);
+                    e.Node.Nodes.Add(node);
+                } 
+            }
+            catch (Exception ex) 
+            { 
+                MessageBox.Show(ex.Message);
+            }
+            SelectedPath = SelectedDrive + SelectedPath;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                if (comboBox2.Text == "LargeIcon")
+                    listView1.View = View.LargeIcon;
+                else if (comboBox2.Text == "Details")
+                    listView1.View = View.Details;
+                else if (comboBox2.Text == "SmallIcon")
+                    listView1.View = View.SmallIcon;
+                else if (comboBox2.Text == "List")
+                    listView1.View = View.List;
+                else listView1.View = View.Tile;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                Process.Start(SelectedPath + "\\" + listView1.FocusedItem.Text);
+            } 
+            catch (Exception ex) { MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_Enter(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void treeView1_Enter(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (File.Exists(SelectedPath + "\\" + textBox1.Text)) 
+                MessageBox.Show("Exists!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
